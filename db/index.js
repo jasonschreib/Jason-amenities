f2f23db015ada0c2a7f51d5765afdc0cefa8dadf
jasonschreib <jwschreiber1@gmail.com>
2021-04-15 21:34:58 -0700

    Hooked up client/server/db

--- db/index.js
+++ db/index.js
@@ -23,35 +23,35 @@ const amenitiesSchema = new mongoose.Schema({
 //compile the schema into a model
 const propertyAmenities = mongoose.model('propertyAmenities', amenitiesSchema);
 
-//function to seed data into the database
-async function seedData() {
-  try {
-    //iterate from 1 to 100
-    for (var i = 1; i <= 100; i++) {
-        //create a new entry for property id - all props will have the same amenities array (at least for now)
-        const doc = new propertyAmenities({
-          propertyID: i,
-          amenities:
-        });
-        //save this entry - it is async!
-        await doc.save();
+// //function to seed data into the database
+// async function seedData() {
+//   try {
+//     //iterate from 1 to 100
+//     for (var i = 1; i <= 100; i++) {
+//         //create a new entry for property id - all props will have the same amenities array (at least for now)
+//         const doc = new propertyAmenities({
+//           propertyID: i,
+//           amenities:
+//         });
+//         //save this entry - it is async!
+//         await doc.save();
 
-    }
-    //console.log message once all done with seeding
-    console.log('finished seeding.');
-  } catch (err) {
-    console.log('Error with seeding', err);
-  }
-};
+//     }
+//     //console.log message once all done with seeding
+//     console.log('finished seeding.');
+//   } catch (err) {
+//     console.log('Error with seeding', err);
+//   }
+// };
 
 //function to retrieve amenities for a certain property
-const retrievePhotoObjs = function (id) {
-  retrieve document with the property id that is passed in
+const retrieveAmenities = function (id) {
+  //retrieve document with the property id that is passed in
   return new Promise((resolve, reject) => {
-    Photo.find({ propertyID: id }, function (err, docs) {
+    propertyAmenities.find({ propertyID: id }, function (err, docs) {
       //err check
       if (err) {
-        console.log('Error with retrieving photos');
+        console.log('Error with retrieving amenities');
         reject(err);
       } else {
         resolve(docs);
---
+++ server/index.js
@@ -0,0 +1,34 @@
+//module for my express server
+const db = require('../db/index.js');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3005;
+
+//serve up initial html page
+app.use(express.static(__dirname + '/../client/dist'));
+
+//serve route to retrieve amenities data for a specific property id
+app.get('/amenities/:propertyID', (req, res) => {
+  //call the db retrieveAmenities function and pass in req and res
+  db.retrieveAmenities(req.params.propertyID)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      res.status(500);
+    });
+});
+
+
+//add one final route to take care of any other endpoints (a catch-all) - important to allow sending back of the index.html
+app.get('*', (req, res) => {
+  //deliver index.html --> this will deliver the bundle too
+  const currPath = path.resolve(__dirname + '/../client/dist/index.html');
+  res.sendFile(currPath);
+});
+
+
+app.listen(port, () => {
+  console.log(`Connected, app listening on port ${port}`);
+})
